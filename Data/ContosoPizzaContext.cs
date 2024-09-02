4b376d1281827a8c96d41f9e300bbe365092ecf9
Uttam Negi <negiuttam17.un@gmail.com>
2024-09-01 21:51:47 -0500

    Added EF core and migrated Pizza Model Glass as Table

---
+++ Data/ContosoPizzaContext.cs
@@ -0,0 +1,16 @@
+﻿using ContosoPizza.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace ContosoPizza.Data
+{
+    public class ContosoPizzaContext :DbContext
+    {
+        public DbSet<Pizza> Pizzas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ContosoPizza;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        }
+    }
+}
